d458ff5bdee32623c6c27953f59bdd8e8e5460ac
Rene Krenn <rkrenn@phoenixctms.org>
2018-02-03 19:49:04 +0100

    et version 1.6.1

--- core/db/dbtool.bat
+++ core/db/dbtool.bat
@@ -1,1 +1,1 @@
-"%JAVA_HOME%\bin\java.exe" -DCTSMS_PROPERTIES="%CTSMS_PROPERTIES%" -Dfile.encoding=Cp1252 -Djava.awt.headless=true -classpath %CATALINA_HOME%\webapps\ctsms-web\WEB-INF\lib\ctsms-core-1.6.0.jar;%CATALINA_HOME%\webapps\ctsms-web\WEB-INF\lib\* org.phoenixctms.ctsms.executable.DBTool %*
+"%JAVA_HOME%\bin\java.exe" -DCTSMS_PROPERTIES="%CTSMS_PROPERTIES%" -Dfile.encoding=Cp1252 -Djava.awt.headless=true -classpath %CATALINA_HOME%\webapps\ctsms-web\WEB-INF\lib\ctsms-core-1.6.1.jar;%CATALINA_HOME%\webapps\ctsms-web\WEB-INF\lib\* org.phoenixctms.ctsms.executable.DBTool %*
--- core/db/dbtool.sh
+++ core/db/dbtool.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-$JRE_HOME/bin/java -DCTSMS_PROPERTIES="$CTSMS_PROPERTIES" -Dfile.encoding=Cp1252 -Djava.awt.headless=true -classpath $CATALINA_HOME/webapps/ctsms-web/WEB-INF/lib/ctsms-core-1.6.0.jar:$CATALINA_HOME/webapps/ctsms-web/WEB-INF/lib/* org.phoenixctms.ctsms.executable.DBTool $*
+$JRE_HOME/bin/java -DCTSMS_PROPERTIES="$CTSMS_PROPERTIES" -Dfile.encoding=Cp1252 -Djava.awt.headless=true -classpath $CATALINA_HOME/webapps/ctsms-web/WEB-INF/lib/ctsms-core-1.6.1.jar:$CATALINA_HOME/webapps/ctsms-web/WEB-INF/lib/* org.phoenixctms.ctsms.executable.DBTool $*
