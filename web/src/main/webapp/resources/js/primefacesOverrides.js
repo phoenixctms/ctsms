phoenixctms/ctsms

commit 20ca9b91d12f186f918be597f97ebc7b5cbdcb46

schedule component: always force monday as first day of week

web/src/main/webapp/resources/js/primefacesOverrides.js
@@ -1455,6 +1455,31 @@ PrimeFaces.widget.AutoComplete.prototype.setValue = function(value) {
 
 }
 
+PrimeFaces.widget.Schedule.prototype.configureLocale = function() {
+	var lang = PrimeFaces.locales[this.cfg.locale];
+
+	this.cfg.weekNumbers = true;
+	this.cfg.weekNumberCalculation = 'iso';
+	this.cfg.weekNumberTitle = 'Week ';
+	this.cfg.firstDay = 1; //force weeks starting on Monday
+	if (lang) {
+		//this.cfg.firstDay = lang.firstDay;
+		this.cfg.monthNames = lang.monthNames;
+		this.cfg.monthNamesShort = lang.monthNamesShort;
+		this.cfg.dayNames = lang.dayNames;
+		this.cfg.dayNamesShort = lang.dayNamesShort;
+		this.cfg.buttonText = {
+		    today : lang.currentText,
+		    month : lang.month,
+		    week : lang.week,
+		    day : lang.day
+		};
+		this.cfg.allDayText = lang.allDayText;
+		this.cfg.weekNumberTitle = lang.calendarWeekText;
+	}
+
+};
+
 /**
  * PrimeFaces FileUpload Widget
  */

web/src/main/webapp/resources/js/scheduleCalendarWeek.js
@@ -1,24 +1,0 @@
-
-PrimeFaces.widget.Schedule.prototype.configureLocale = function() {
-	var lang = PrimeFaces.locales[this.cfg.locale];
-
-	this.cfg.weekNumbers = true;
-	this.cfg.weekNumberCalculation = 'iso';
-	this.cfg.weekNumberTitle = 'Week ';
-	if (lang) {
-		this.cfg.firstDay = lang.firstDay;
-		this.cfg.monthNames = lang.monthNames;
-		this.cfg.monthNamesShort = lang.monthNamesShort;
-		this.cfg.dayNames = lang.dayNames;
-		this.cfg.dayNamesShort = lang.dayNamesShort;
-		this.cfg.buttonText = {
-		    today : lang.currentText,
-		    month : lang.month,
-		    week : lang.week,
-		    day : lang.day
-		};
-		this.cfg.allDayText = lang.allDayText;
-		this.cfg.weekNumberTitle = lang.calendarWeekText;
-	}
-
-};

commit 3f9c58fbaa16f2a5f91024ab5099441dd77512d9

fix js error for pages w/o schedule component
regression from 20ca9b91d12f186f918be597f97ebc7b5cbdcb46

prevents i.e. progressbars from initializing properly.

web/src/main/webapp/resources/js/primefacesOverrides.js
@@ -1455,30 +1455,32 @@ PrimeFaces.widget.AutoComplete.prototype.setValue = function(value) {
 
 }
 
-PrimeFaces.widget.Schedule.prototype.configureLocale = function() {
-	var lang = PrimeFaces.locales[this.cfg.locale];
-
-	this.cfg.weekNumbers = true;
-	this.cfg.weekNumberCalculation = 'iso';
-	this.cfg.weekNumberTitle = 'Week ';
-	this.cfg.firstDay = 1; //force weeks starting on Monday
-	if (lang) {
-		//this.cfg.firstDay = lang.firstDay;
-		this.cfg.monthNames = lang.monthNames;
-		this.cfg.monthNamesShort = lang.monthNamesShort;
-		this.cfg.dayNames = lang.dayNames;
-		this.cfg.dayNamesShort = lang.dayNamesShort;
-		this.cfg.buttonText = {
-		    today : lang.currentText,
-		    month : lang.month,
-		    week : lang.week,
-		    day : lang.day
-		};
-		this.cfg.allDayText = lang.allDayText;
-		this.cfg.weekNumberTitle = lang.calendarWeekText;
-	}
-
-};
+if (PrimeFaces.widget.Schedule) {
+	PrimeFaces.widget.Schedule.prototype.configureLocale = function() {
+		var lang = PrimeFaces.locales[this.cfg.locale];
+	
+		this.cfg.weekNumbers = true;
+		this.cfg.weekNumberCalculation = 'iso';
+		this.cfg.weekNumberTitle = 'Week ';
+		this.cfg.firstDay = 1; //force weeks starting on Monday
+		if (lang) {
+			//this.cfg.firstDay = lang.firstDay;
+			this.cfg.monthNames = lang.monthNames;
+			this.cfg.monthNamesShort = lang.monthNamesShort;
+			this.cfg.dayNames = lang.dayNames;
+			this.cfg.dayNamesShort = lang.dayNamesShort;
+			this.cfg.buttonText = {
+			    today : lang.currentText,
+			    month : lang.month,
+			    week : lang.week,
+			    day : lang.day
+			};
+			this.cfg.allDayText = lang.allDayText;
+			this.cfg.weekNumberTitle = lang.calendarWeekText;
+		}
+	
+	};
+}
 
 /**
  * PrimeFaces FileUpload Widget