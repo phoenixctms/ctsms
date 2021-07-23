f966c15ed71ffcacd8f312645b2190920974107a
Rene Krenn <rkrenn@phoenixctms.org>
2021-07-23 13:27:36 +0200

    parent user inheritance - generated finder&service method testcase stubs

---
+++ core/src/test/java/org/phoenixctms/ctsms/service/user/test/UserService_getInheritedUserTest.java
@@ -0,0 +1,41 @@
+// This file is part of the Phoenix CTMS project (www.phoenixctms.org),
+// distributed under LGPL v2.1. Copyright (C) 2011 - 2017.
+//
+package org.phoenixctms.ctsms.service.user.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * <p>
+ * Test case for method <code>getInheritedUser</code> of service <code>UserService</code>.
+ * </p>
+ *
+ * @see org.phoenixctms.ctsms.service.user.UserService#getInheritedUser(org.phoenixctms.ctsms.vo.AuthenticationVO, java.lang.Long)
+ */
+@Test(groups={"service","UserService"})
+public class UserService_getInheritedUserTest extends UserServiceBaseTest {
+
+  /**
+   * Test succes path for service method <code>getInheritedUser</code>
+   *
+   * Tests expected behaviour of service method.
+   */
+  @Test
+  public void testSuccessPath() {
+    Assert.fail( "Test 'UserService_getInheritedUserTest.testSuccessPath()}' not implemented." );
+  }
+
+  /*
+   * Add test methods for each test case of the 'UserService.org.andromda.cartridges.spring.metafacades.SpringServiceOperationLogicImpl[org.phoenixctms.ctsms.service.user.UserService.getInheritedUser]()' service method.
+   */
+
+  /**
+   * Test special case XYZ for service method <code></code>
+   */
+  /*
+  @Test
+  public void testCaseXYZ() {
+  }
+  */
+}
