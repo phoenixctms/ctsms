1ddbfa64078e038972838adbef4bb67043fee3b1
Rene Krenn <rkrenn@phoenixctms.org>
2021-09-24 12:13:23 +0200

    use RSA/ECB/PKCS1Padding for padded RSA encryption
    OEAP rejects using private key to encypt/public key to decrypt

--- core/src/main/java/org/phoenixctms/ctsms/security/CryptoUtil.java
+++ core/src/main/java/org/phoenixctms/ctsms/security/CryptoUtil.java
@@ -53,7 +53,7 @@ public final class CryptoUtil {
 	private static final int SYMMETRIC_KEY_LENGTH = 128;
 	private static final int SALT_LENGTH = 16;
 	private static final String ASYMMETRIC_ALGORITHM = "RSA";
-	private static final String PADDED_ASYMMETRIC_ALGORITHM = ASYMMETRIC_ALGORITHM + "/None/OAEPWITHSHA-256ANDMGF1PADDING";
+	private static final String PADDED_ASYMMETRIC_ALGORITHM = ASYMMETRIC_ALGORITHM + "/ECB/PKCS1Padding";
 	private static final int ASYMMETRIC_KEY_LENGTH = 1024;
 	public static final String SIGNATURE_ALGORITHM = "SHA1withRSA";
 	public static final int SEED = 100; // (new Random()).nextInt(100);
