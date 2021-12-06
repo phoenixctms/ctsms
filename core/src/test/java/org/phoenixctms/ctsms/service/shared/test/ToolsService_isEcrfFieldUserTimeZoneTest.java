14d1eb5c559b4b007a977855ecd45bbd711d1a0e
Rene Krenn <rkrenn@phoenixctms.org>
2021-12-06 08:17:11 +0100

    toolService.is*UserTimezone() service methods - test method stubs

---
+++ core/src/test/java/org/phoenixctms/ctsms/service/shared/test/ToolsService_isEcrfFieldUserTimeZoneTest.java
@@ -0,0 +1,41 @@
+// This file is part of the Phoenix CTMS project (www.phoenixctms.org),
+// distributed under LGPL v2.1. Copyright (C) 2011 - 2017.
+//
+package org.phoenixctms.ctsms.service.shared.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * <p>
+ * Test case for method <code>isEcrfFieldUserTimeZone</code> of service <code>ToolsService</code>.
+ * </p>
+ *
+ * @see org.phoenixctms.ctsms.service.shared.ToolsService#isEcrfFieldUserTimeZone(java.lang.Long)
+ */
+@Test(groups={"service","ToolsService"})
+public class ToolsService_isEcrfFieldUserTimeZoneTest extends ToolsServiceBaseTest {
+
+  /**
+   * Test succes path for service method <code>isEcrfFieldUserTimeZone</code>
+   *
+   * Tests expected behaviour of service method.
+   */
+  @Test
+  public void testSuccessPath() {
+    Assert.fail( "Test 'ToolsService_isEcrfFieldUserTimeZoneTest.testSuccessPath()}' not implemented." );
+  }
+
+  /*
+   * Add test methods for each test case of the 'ToolsService.org.andromda.cartridges.spring.metafacades.SpringServiceOperationLogicImpl[org.phoenixctms.ctsms.service.shared.ToolsService.isEcrfFieldUserTimeZone]()' service method.
+   */
+
+  /**
+   * Test special case XYZ for service method <code></code>
+   */
+  /*
+  @Test
+  public void testCaseXYZ() {
+  }
+  */
+}
