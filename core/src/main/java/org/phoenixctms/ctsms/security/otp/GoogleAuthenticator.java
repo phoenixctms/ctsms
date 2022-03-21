78d1171186aaeb589977ab98a2c52d767d445136
Rene Krenn <rkrenn@phoenixctms.org>
2022-03-21 03:43:27 +0100

    refactor GoogleAuthenticator - sendOTP(), verifyOTP()

--- core/src/main/java/org/phoenixctms/ctsms/security/otp/GoogleAuthenticator.java
+++ core/src/main/java/org/phoenixctms/ctsms/security/otp/GoogleAuthenticator.java
@@ -3,14 +3,16 @@ package org.phoenixctms.ctsms.security.otp;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.Map;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 
 import org.apache.commons.codec.binary.Base32;
+import org.phoenixctms.ctsms.domain.Password;
 import org.phoenixctms.ctsms.domain.PasswordDao;
-import org.phoenixctms.ctsms.domain.User;
 import org.phoenixctms.ctsms.security.CryptoUtil;
+import org.phoenixctms.ctsms.util.CoreUtil;
 import org.phoenixctms.ctsms.util.DefaultSettings;
 import org.phoenixctms.ctsms.util.SettingCodes;
 import org.phoenixctms.ctsms.util.Settings;
@@ -21,9 +23,11 @@ public class GoogleAuthenticator extends OTPAuthenticator {
 	private static final String SECRET_RANDOM_ALGORITHM = "SHA1PRNG";
 	private static final int SECRET_LENGTH = 20;
 	private static final String MAC_ALGORITHM = "HmacSHA1";
-	private PasswordDao passwordDao = null;
+	private PasswordDao XXXXpasswordDao = null;
 
+	@Override
 	public String createOtpSecret() throws NoSuchAlgorithmException {
+		//128bits at least. we dont use scratch codes.
 		SecureRandom rand = SecureRandom.getInstance(SECRET_RANDOM_ALGORITHM);
 		byte[] secret = new byte[SECRET_LENGTH];
 		rand.nextBytes(secret);
@@ -31,13 +35,30 @@ public class GoogleAuthenticator extends OTPAuthenticator {
 		return codec.encodeToString(secret);
 	}
 
-	public boolean verifyOtp(User user, String plainPassword, String code) throws Exception {
-		return verifyOtp(CryptoUtil.decryptOtpSecret(passwordDao.findLastPassword(user.getId()), plainPassword), Long.parseLong(code), System.currentTimeMillis());
+	@Override
+	public boolean verifyOtp(String otpSecret, String otpEntered, String otpSent) throws InvalidKeyException, NumberFormatException, NoSuchAlgorithmException {
+		return verifyOtp(otpSecret, Long.parseLong(otpEntered), System.currentTimeMillis());
 	}
 
+	@Override
+	public String getOtpRegistrationInfo(Password password, String plainDepartmentPassword) throws Exception {
+		Map messageParameters = CoreUtil.createEmptyTemplateModel();
+		String otpSecret = null;
+		if (password != null) {
+			otpSecret = CryptoUtil.decryptOtpSecret(password, CryptoUtil.decryptPassword(password, plainDepartmentPassword));
+		}
+		if (otpSecret != null) {
+			messageParameters.put(OTPRegistrationInfoMessageTemplateParameters.OTP_SECRET, otpSecret);
+		}
+		Map model = createTemplateModel(messageParameters);
+		return getOtpPRegistrationInfoMessage(model);
+	}
+
+	//https://thegreyblog.blogspot.com/2011/12/google-authenticator-using-it-in-your.html
 	private static boolean verifyOtp(String otpSecret, long code, long time) throws NoSuchAlgorithmException, InvalidKeyException {
 		Base32 codec = new Base32();
 		byte[] secret = codec.decode(otpSecret);
+		//the window in time steps to tolerate for clock skew:
 		int window = Settings.getInt(SettingCodes.GOOGLE_AUTHENTICATOR_WINDOW, Bundle.SETTINGS, DefaultSettings.GOOGLE_AUTHENTICATOR_WINDOW);
 		for (int i = -((window - 1) / 2); i <= window / 2; ++i) {
 			long hash = verifyOtp(secret,
@@ -49,9 +70,10 @@ public class GoogleAuthenticator extends OTPAuthenticator {
 		return false;
 	}
 
+	//https://thegreyblog.blogspot.com/2011/12/google-authenticator-using-it-in-your.html
 	private static int verifyOtp(byte[] secret, long t) throws NoSuchAlgorithmException, InvalidKeyException {
 		byte[] data = new byte[8];
-		long value = t;
+		long value = t; //the current time in steps
 		for (int i = 8; i-- > 0; value >>>= 8) {
 			data[i] = (byte) value;
 		}
@@ -70,11 +92,7 @@ public class GoogleAuthenticator extends OTPAuthenticator {
 		return (int) truncatedHash;
 	}
 
-	public PasswordDao getPasswordDao() {
-		return passwordDao;
-	}
-
 	public void setPasswordDao(PasswordDao passwordDao) {
-		this.passwordDao = passwordDao;
+		this.XXXXpasswordDao = passwordDao;
 	}
 }
