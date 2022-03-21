4b40570c4899a437f46fdb8b80bff2a5f5901736
Rene Krenn <rkrenn@phoenixctms.org>
2022-03-21 03:45:02 +0100

    constants for data in OTP auth service description vsl templates

---
+++ core/src/main/java/org/phoenixctms/ctsms/security/otp/OTPRegistrationInfoMessageTemplateParameters.java
@@ -0,0 +1,8 @@
+package org.phoenixctms.ctsms.security.otp;
+
+public interface OTPRegistrationInfoMessageTemplateParameters {
+	//	public static final String TEMPLATE_MODEL_FIELD_NAME_ASSOCIATION_PATH_SEPARATOR = "_";
+	//	public static final boolean TEMPLATE_MODEL_LOWER_CASE_FIELD_NAMES = true;
+
+	public static final String OTP_SECRET = "otp_secret";
+}
