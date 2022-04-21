3cbe0ed16d6bfddb0412684b593ea656c4ecc4f2
Rene Krenn <rkrenn@phoenixctms.org>
2022-04-21 19:51:50 +0200

    schema versioning: schema-up-<tag>.sql

--- core/db/schema-up-190.sql
+++
@@ -1,8 +0,0 @@
-
-alter table PASSWORD add column ENABLE2FA BOOLEAN;
-update PASSWORD set ENABLE2FA = 'f';
-alter table PASSWORD alter ENABLE2FA set not null;
-
-alter table PASSWORD add column SHOW_OTP_REGISTRATION_INFO BOOLEAN;
-update PASSWORD set SHOW_OTP_REGISTRATION_INFO = 'f';
-alter table PASSWORD alter SHOW_OTP_REGISTRATION_INFO set not null;
