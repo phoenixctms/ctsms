d00bf6482f6afb9ba7550294875d5e6cae8483e2
Rene Krenn <rkrenn@phoenixctms.org>
2022-04-21 19:51:04 +0200

    schema versioning: schema-set-version.sql

---
+++ core/db/schema-set-version.sql
@@ -0,0 +1,15 @@
+
+create or replace function get_database_version()
+returns text as $$
+  select description from pg_shdescription join pg_database on objoid = pg_database.oid where datname = 'ctsms' limit 1;
+$$ language sql stable;
+
+create or replace function set_database_version(text)
+returns void as $$
+begin
+  raise notice 'database schema version set to %', $1;
+  execute format('comment on database ctsms is %L',$1);
+end
+$$ language plpgsql;
+
+select set_database_version('010800001');
