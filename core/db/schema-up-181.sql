a64856548ca44eb4923abb3ab0f3d6c95928d42f
Rene Krenn <rkrenn@phoenixctms.org>
2022-04-25 19:37:46 +0200

    add auto-ddl statements for 1.8.1

--- core/db/schema-up-181.sql
+++ core/db/schema-up-181.sql
@@ -1,8 +1,12 @@
-
 alter table PASSWORD add column ENABLE2FA BOOLEAN;
 update PASSWORD set ENABLE2FA = 'f';
 alter table PASSWORD alter ENABLE2FA set not null;
-  
+
+alter table PASSWORD add column OTP_TYPE CHARACTER VARYING(1024);
+alter table PASSWORD add column OTP_SECRET_SALT BYTEA unique;
+alter table PASSWORD add column OTP_SECRET_IV BYTEA unique;
+alter table PASSWORD add column ENCRYPTED_OTP_SECRET BYTEA unique;
+
 alter table PASSWORD add column SHOW_OTP_REGISTRATION_INFO BOOLEAN;
 update PASSWORD set SHOW_OTP_REGISTRATION_INFO = 'f';
 alter table PASSWORD alter SHOW_OTP_REGISTRATION_INFO set not null;
