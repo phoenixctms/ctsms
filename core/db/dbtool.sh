316e9239ec85f2cbcff9a15626d217b7bd6914ed
Rene Krenn <rkrenn@phoenixctms.org>
2022-05-17 12:22:42 +0200

    possibility to run the dbtool from postgresql stored procedures

--- core/db/dbtool.sh
+++ core/db/dbtool.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-$JRE_HOME/bin/java -DCTSMS_PROPERTIES="$CTSMS_PROPERTIES" -DCTSMS_JAVA="$CTSMS_JAVA" -Dfile.encoding=Cp1252 -Djava.awt.headless=true -classpath $CATALINA_HOME/webapps/ctsms-web/WEB-INF/lib/ctsms-core-1.78.0.jar:$CATALINA_HOME/webapps/ctsms-web/WEB-INF/lib/* org.phoenixctms.ctsms.executable.DBTool $*
+CTSMS_PROPERTIES=${CTSMS_PROPERTIES:-/ctsms/properties}
+CTSMS_JAVA=${CTSMS_JAVA:-/ctsms/java}
+java -DCTSMS_PROPERTIES="$CTSMS_PROPERTIES" -DCTSMS_JAVA="$CTSMS_JAVA" -Dfile.encoding=Cp1252 -Djava.awt.headless=true -Xms2048m -Xmx4096m -Xss256k -XX:+UseParallelGC -XX:MaxGCPauseMillis=1500 -XX:GCTimeRatio=9 -XX:+CMSClassUnloadingEnabled -XX:ReservedCodeCacheSize=256m -classpath /ctsms/build/ctsms/web/target/ctsms-1.8.1/WEB-INF/lib/ctsms-core-1.8.1.jar:/ctsms/build/ctsms/web/target/ctsms-1.8.1/WEB-INF/lib/* org.phoenixctms.ctsms.executable.DBTool $*
