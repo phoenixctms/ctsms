cabaafdbc64262a6018ad7ed11ca0c0d2b45bc4c
Rene Krenn <rkrenn@phoenixctms.org>
2022-06-21 22:55:56 +0200

    test launching tomcat #11

--- .github/workflows/seleniumtest.pl
+++ .github/workflows/seleniumtest.pl
@@ -2,6 +2,6 @@ use Selenium::Remote::Driver;
  
 my $driver = Selenium::Remote::Driver->new('browser_name' => 'chrome');
 $driver->get('http://localhost:8080');
-print $driver->get_title();
+print $driver->get_body();
 $driver->quit();
 
