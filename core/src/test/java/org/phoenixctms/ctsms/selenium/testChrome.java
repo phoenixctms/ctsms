8b0f497bac371db280893f43bb9f0b83dacbd653
Rene Krenn <rkrenn@phoenixctms.org>
2022-06-30 16:43:49 +0200

    test launch java selenium remote #11

--- core/src/test/java/org/phoenixctms/ctsms/selenium/testChrome.java
+++ core/src/test/java/org/phoenixctms/ctsms/selenium/testChrome.java
@@ -1,6 +1,7 @@
 package org.phoenixctms.ctsms.selenium;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 //https://tecadmin.net/setup-selenium-chromedriver-on-ubuntu/
 public class testChrome {
@@ -8,7 +9,10 @@ public class testChrome {
 	public static void main(String[] args) {
 		//String pathToChromeDriver = "C:\\chromedriver_win32\\chromedriver.exe";
 		//System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-		ChromeDriver Chrom1 = new ChromeDriver();
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.addArguments("--headless");
+		chromeOptions.addArguments("--no-sandbox");
+		ChromeDriver Chrom1 = new ChromeDriver(chromeOptions);
 		Chrom1.get("http://localhost:8080");
 	}
 }
