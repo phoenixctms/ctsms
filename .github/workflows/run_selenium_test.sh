de21432607e7e72b531afa842ce83e51dffa6d4d
Rene Krenn <rkrenn@phoenixctms.org>
2022-07-01 13:51:01 +0200

    test launch java selenium remote (#241)

---
+++ .github/workflows/run_selenium_test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo $JAVA_HOME/bin/java -Dfile.encoding=Cp1252 -Dwebdriver.chrome.driver=/usr/bin/chromedriver -classpath /home/runner/work/ctsms/ctsms/core/target/test-classes/:/home/runner/work/ctsms/ctsms/selenium-server-standalone-3.141.59.jar $*
+#javac -classpath /home/runner/work/ctsms/ctsms/selenium-server-standalone-3.141.59.jar /home/runner/work/ctsms/ctsms/core/src/test/java/org/phoenixctms/ctsms/selenium/$1.java
+#sudo java -Dwebdriver.chrome.driver=/usr/bin/chromedriver -classpath /home/runner/work/ctsms/ctsms/selenium-server-standalone-3.141.59.jar:/home/runner/work/ctsms/ctsms/core/src/test/java/ org.phoenixctms.ctsms.selenium.$1
