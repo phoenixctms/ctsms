bc20cc088becffbbb9fff9f79937a4b0489a9ff9
Rene Krenn <rkrenn@phoenixctms.org>
2022-09-23 14:01:09 +0200

    expose surefireReportsDirectory property to maven

--- .github/workflows/run_selenium_test.sh
+++ .github/workflows/run_selenium_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mvn surefire:test -Dtest=$1 -DskipTests=false -Dwebdriver.chrome.driver="/usr/bin/chromedriver" -Dctsms.base.url="http://localhost:8080" -Dctsms.log.path="/home/runner/work/ctsms/testoutput" -DoutputDirectory="/home/runner/work/ctsms/testoutput"
+mvn surefire:test -Dtest=$1 -DskipTests=false -Dwebdriver.chrome.driver="/usr/bin/chromedriver" -Dctsms.base.url="http://localhost:8080" -Dctsms.log.path="/home/runner/work/ctsms/testoutput" -DsurefireReportsDirectory="/home/runner/work/ctsms/testoutput" --no-transfer-progress
 #sudo $JAVA_HOME/bin/java -Dfile.encoding=Cp1252 -Dwebdriver.chrome.driver=/usr/bin/chromedriver -classpath /home/runner/work/ctsms/ctsms/core/target/test-classes/:/home/runner/work/ctsms/ctsms/selenium-server-standalone-4.4.0.jar $*
 #javac -classpath /home/runner/work/ctsms/ctsms/selenium-server-standalone-3.141.59.jar /home/runner/work/ctsms/ctsms/core/src/test/java/org/phoenixctms/ctsms/selenium/$1.java
 #sudo java -Dwebdriver.chrome.driver=/usr/bin/chromedriver -classpath /home/runner/work/ctsms/ctsms/selenium-server-standalone-3.141.59.jar:/home/runner/work/ctsms/ctsms/core/src/test/java/ org.phoenixctms.ctsms.selenium.$1
