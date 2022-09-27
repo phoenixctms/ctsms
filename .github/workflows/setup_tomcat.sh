44d1e9cdef701c158b6c699c5046c07ae586afe1
Rene Krenn <rkrenn@phoenixctms.org>
2022-09-27 02:01:36 +0200

    consider CTSMS_PROPERTIES in tomcat #7

--- .github/workflows/setup_tomcat.sh
+++ .github/workflows/setup_tomcat.sh
@@ -2,6 +2,12 @@
 
 apt-get -q -y install tomcat9
 systemctl stop tomcat9
+echo 'CTSMS_PROPERTIES=/ctsms/properties' >>/etc/default/tomcat9
+echo 'CTSMS_JAVA=/ctsms/java' >>/etc/default/tomcat9
+sed -r -i "s|# Lifecycle|EnvironmentFile=/etc/default/tomcat9\\n\\n# Lifecycle|" /etc/systemd/system/multi-user.target.wants/tomcat9.service
+sed -r -i "s|# Security|# Security\\nReadWritePaths=/ctsms/external_files/|" /etc/systemd/system/multi-user.target.wants/tomcat9.service
+cat /etc/default/tomcat9
+cat /etc/systemd/system/multi-user.target.wants/tomcat9.service
 VERSION=$(grep -oP '<application.version>\K[^<]+' /home/runner/work/ctsms/ctsms/pom.xml)
 #sed -r -i "s/^JAVA_OPTS.+/JAVA_OPTS=\"-server -Djava.awt.headless=true -Xms$XMS -Xmx$XMX -Xss$XSS -XX:+UseParallelGC -XX:MaxGCPauseMillis=1500 -XX:GCTimeRatio=9 -XX:+CMSClassUnloadingEnabled -XX:ReservedCodeCacheSize=$PERM\"/" /etc/default/tomcat9
 echo 'CTSMS_PROPERTIES=/ctsms/properties' >>/etc/default/tomcat9
