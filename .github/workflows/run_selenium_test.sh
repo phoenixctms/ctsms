4a1a5eeed808f69620935243610e491d42b4588b
Rene Krenn <rkrenn@phoenixctms.org>
2022-10-06 17:26:10 +0200

    track version, git branch and commit id in test result emails #1

--- .github/workflows/run_selenium_test.sh
+++ .github/workflows/run_selenium_test.sh
@@ -2,7 +2,7 @@
 CTSMS_PROPERTIES=/ctsms/properties
 CTSMS_JAVA=/ctsms/java
 VERSION=$(grep -oP '<application.version>\K[^<]+' /home/runner/work/ctsms/ctsms/pom.xml)
-BRANCH=$(git symbolic-ref --short HEAD)
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
 COMMIT=$(git rev-parse --short HEAD)
 mvn surefire:test -DCTSMS_PROPERTIES="$CTSMS_PROPERTIES" -DCTSMS_JAVA="$CTSMS_JAVA" -Dtest=$1 -DskipTests=false -Dwebdriver.chrome.driver="/usr/bin/chromedriver" -Dctsms.test.baseurl="http://localhost:8080" -Dctsms.test.directory="/home/runner/work/ctsms/testoutput" -DsurefireReportsDirectory="/home/runner/work/ctsms/testoutput" -Dctsms.test.windowsize="2000,3000" -Dctsms.test.emailrecipients="$2" --no-transfer-progress -Dgithub.workflow.url=https://github.com/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID -Dctsms.test.version=$VERSION -Dgit.branch=$BRANCH -Dgit.commit=$COMMIT
 #sudo $JAVA_HOME/bin/java -Dfile.encoding=Cp1252 -Dwebdriver.chrome.driver=/usr/bin/chromedriver -classpath /home/runner/work/ctsms/ctsms/core/target/test-classes/:/home/runner/work/ctsms/ctsms/selenium-server-standalone-4.4.0.jar $*
