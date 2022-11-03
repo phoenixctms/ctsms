1440125d5927a6f99d6f6e7a3899e0344e9f342f
Rene Krenn <rkrenn@phoenixctms.org>
2022-11-04 00:07:59 +0100

    show commit id in application version #6

--- .github/workflows/dbtool.sh
+++ .github/workflows/dbtool.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-printenv
 #VERSION=$(grep -oP '<application.version>\K[^<]+' /home/runner/work/ctsms/ctsms/pom.xml)
 #CTSMS_PROPERTIES=/ctsms/properties
 #CTSMS_JAVA=/ctsms/java
-$JAVA_HOME/bin/java -DCTSMS_PROPERTIES="$CTSMS_PROPERTIES" -DCTSMS_JAVA="$CTSMS_JAVA" -Dfile.encoding=Cp1252 -Djava.awt.headless=true -classpath /home/runner/work/ctsms/ctsms/web/target/ctsms-$VERSION/WEB-INF/lib/ctsms-core-$VERSION.jar:/home/runner/work/ctsms/ctsms/web/target/ctsms-$VERSION/WEB-INF/lib/* org.phoenixctms.ctsms.executable.DBTool $*
+$JAVA_HOME/bin/java -DCTSMS_PROPERTIES="${{ env.CTSMS_PROPERTIES }}" -DCTSMS_JAVA="${{ env.CTSMS_JAVA }}" -Dfile.encoding=Cp1252 -Djava.awt.headless=true -classpath /home/runner/work/ctsms/ctsms/web/target/ctsms-$VERSION/WEB-INF/lib/ctsms-core-$VERSION.jar:/home/runner/work/ctsms/ctsms/web/target/ctsms-${{ env.VERSION }}/WEB-INF/lib/* org.phoenixctms.ctsms.executable.DBTool $*
