82232e953dce2d28b5ea8dedbbc642c8bbb168ae
Rene Krenn <rkrenn@phoenixctms.org>
2023-02-15 14:02:08 +0100

    move github action runner /ctsms folder to /home/runner/work/ctsms #7

--- .github/workflows/download_masterdata.sh
+++ .github/workflows/download_masterdata.sh
@@ -8,8 +8,9 @@ wget --no-verbose https://api.github.com/repos/phoenixctms/master-data/tarball/m
 mkdir /home/runner/work/ctsms/master_data
 tar -zxvf /home/runner/work/ctsms/master-data.tar.gz -C /home/runner/work/ctsms/master_data --strip-components 1
 rm /home/runner/work/ctsms/master-data.tar.gz -f
-chown ctsms:ctsms /home/runner/work/ctsms -R
-chmod 777 /home/runner/work/ctsms -R
 
 echo "external_file_data_dir=/home/runner/work/ctsms/external_files" >> /home/runner/work/ctsms/properties/ctsms-settings.properties
 sed -r -i "s/dbtool_lock_file_name=[^=]+/dbtool_lock_file_name=\/home\/runner\/work\/ctsms\/{0}.lock/" /home/runner/work/ctsms/properties/ctsms-dbtool.properties
+
+chown ctsms:ctsms /home/runner/work/ctsms -R
+chmod 777 /home/runner/work/ctsms -R
