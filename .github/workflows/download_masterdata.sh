541dafc5d94227c528d32c433eeffb557775da76
Rene Krenn <rkrenn@phoenixctms.org>
2023-02-15 16:46:49 +0100

    move github action runner /ctsms folder to /home/runner/work/ctsms #13

--- .github/workflows/download_masterdata.sh
+++ .github/workflows/download_masterdata.sh
@@ -9,12 +9,12 @@ mkdir /home/runner/work/ctsms/master_data
 tar -zxvf /home/runner/work/ctsms/master-data.tar.gz -C /home/runner/work/ctsms/master_data --strip-components 1
 rm /home/runner/work/ctsms/master-data.tar.gz -f
 
-echo "\nexternal_file_data_dir=/home/runner/work/ctsms/external_files" >> /home/runner/work/ctsms/properties/ctsms-settings.properties
+echo "external_file_data_dir=/home/runner/work/ctsms/external_files" >> /home/runner/work/ctsms/properties/ctsms-settings.properties
 
-echo "\ndb_tool=/home/runner/work/ctsms/ctsms/.github/workflows/dbtool.sh" >> /home/runner/work/ctsms/properties/ctsms-settings.properties
-echo "\necrf_exporter_process_pl=perl /home/runner/work/ctsms/bulk_processor/CTSMS/BulkProcessor/Projects/ETL/EcrfExporter/process.pl --config=config.job.cfg" >> /home/runner/work/ctsms/properties/ctsms-settings.properties
-echo "\necrf_importer_process_pl=perl /home/runner/work/ctsms/bulk_processor/CTSMS/BulkProcessor/Projects/ETL/EcrfImporter/process.pl --config=config.job.cfg" >> /home/runner/work/ctsms/properties/ctsms-settings.properties
-echo "\ninquiry_exporter_process_pl=perl /home/runner/work/ctsms/bulk_processor/CTSMS/BulkProcessor/Projects/ETL/InquiryExporter/process.pl --config=config.job.cfg" >> /home/runner/work/ctsms/properties/ctsms-settings.properties
+echo "db_tool=/home/runner/work/ctsms/ctsms/.github/workflows/dbtool.sh" >> /home/runner/work/ctsms/properties/ctsms-settings.properties
+echo "ecrf_exporter_process_pl=perl /home/runner/work/ctsms/bulk_processor/CTSMS/BulkProcessor/Projects/ETL/EcrfExporter/process.pl --config=config.job.cfg" >> /home/runner/work/ctsms/properties/ctsms-settings.properties
+echo "ecrf_importer_process_pl=perl /home/runner/work/ctsms/bulk_processor/CTSMS/BulkProcessor/Projects/ETL/EcrfImporter/process.pl --config=config.job.cfg" >> /home/runner/work/ctsms/properties/ctsms-settings.properties
+echo "inquiry_exporter_process_pl=perl /home/runner/work/ctsms/bulk_processor/CTSMS/BulkProcessor/Projects/ETL/InquiryExporter/process.pl --config=config.job.cfg" >> /home/runner/work/ctsms/properties/ctsms-settings.properties
 
 sed -r -i "s/dbtool_lock_file_name=[^=]+/dbtool_lock_file_name=\/home\/runner\/work\/ctsms\/{0}.lock/" /home/runner/work/ctsms/properties/ctsms-dbtool.properties
 
