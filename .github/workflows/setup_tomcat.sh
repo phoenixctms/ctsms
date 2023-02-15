7907e7d0758cb0a5827e860a75efdfa02d97704d
Rene Krenn <rkrenn@phoenixctms.org>
2023-02-15 17:55:22 +0100

    move github action runner /ctsms folder to /home/runner/work/ctsms #16

--- .github/workflows/setup_tomcat.sh
+++ .github/workflows/setup_tomcat.sh
@@ -6,6 +6,7 @@ systemctl stop tomcat9
 #sed -r -i "s/^JAVA_OPTS.+/JAVA_OPTS=\"-server -Djava.awt.headless=true -Xms$XMS -Xmx$XMX -Xss$XSS -XX:+UseParallelGC -XX:MaxGCPauseMillis=1500 -XX:GCTimeRatio=9 -XX:+CMSClassUnloadingEnabled -XX:ReservedCodeCacheSize=$PERM\"/" /etc/default/tomcat9
 echo "CTSMS_PROPERTIES=$CTSMS_PROPERTIES" >>/etc/default/tomcat9
 echo "CTSMS_JAVA=$CTSMS_JAVA" >>/etc/default/tomcat9
+echo "ProtectSystem=false" >>/usr/lib/systemd/system/tomcat9.service
 sed -r -i "s|# Lifecycle|EnvironmentFile=/etc/default/tomcat9\\n\\n# Lifecycle|" /usr/lib/systemd/system/tomcat9.service
 sed -r -i "s|# Security|# Security\\nReadWritePaths=/home/runner/work/ctsms/external_files/|" /usr/lib/systemd/system/tomcat9.service
 chmod 755 /home/runner/work/ctsms/ctsms/web/target/ctsms-$VERSION.war
@@ -15,3 +16,4 @@ systemctl daemon-reload
 systemctl start tomcat9
 
 cat /etc/default/tomcat9
+cat /usr/lib/systemd/system/tomcat9.service
