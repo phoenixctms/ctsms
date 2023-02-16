e1714aa3954138a11b40e33d30a5632ea0ebbe9b
Rene Krenn <rkrenn@phoenixctms.org>
2023-02-16 11:24:16 +0100

    run jobrunner dbtool in /ctsms folder, launch from tomcat #4

--- core/src/main/java/org/phoenixctms/ctsms/util/CoreUtil.java
+++ core/src/main/java/org/phoenixctms/ctsms/util/CoreUtil.java
@@ -1000,6 +1000,7 @@ public final class CoreUtil implements ApplicationContextAware {
 					Settings.getString(SettingCodes.ECRF_IMPORTER_PROCESS_PL, Bundle.SETTINGS, DefaultSettings.ECRF_IMPORTER_PROCESS_PL));
 			return runProcess(command, blocking);
 		} catch (Exception e) {
+			System.out.println(e.getMessage());
 			throw new IllegalArgumentException(L10nUtil.getMessage(MessageCodes.START_JOB_ERROR, DefaultMessages.START_JOB_ERROR,
 					new Object[] { e.getMessage() }));
 		}
