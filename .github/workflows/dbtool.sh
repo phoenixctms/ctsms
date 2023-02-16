2fa01a931e89ace4332d53c772cd56c671f1251d
Rene Krenn <rkrenn@phoenixctms.org>
2023-02-16 15:13:48 +0100

    run jobrunner dbtool in /ctsms folder, launch from tomcat #14

--- .github/workflows/dbtool.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#VERSION=$(grep -oP '<application.version>\K[^<]+' /home/runner/work/ctsms/ctsms/pom.xml)
-CTSMS_PROPERTIES=/ctsms/properties
-CTSMS_JAVA=/ctsms/java
-VERSION=1.8.1
-#CTSMS_PROPERTIES=${CTSMS_PROPERTIES:-/ctsms/properties}
-#CTSMS_JAVA=${CTSMS_JAVA:-/ctsms/java}
-$JAVA_HOME/bin/java -DCTSMS_PROPERTIES="$CTSMS_PROPERTIES" -DCTSMS_JAVA="$CTSMS_JAVA" -Dfile.encoding=Cp1252 -Djava.awt.headless=true -classpath /home/runner/work/ctsms/ctsms/web/target/ctsms-$VERSION/WEB-INF/lib/ctsms-core-$VERSION.jar:/home/runner/work/ctsms/ctsms/web/target/ctsms-$VERSION/WEB-INF/lib/* org.phoenixctms.ctsms.executable.DBTool $*
