ab44649a2b41c65edb03ca2a24175e6df256cebe
Rene Krenn <rkrenn@phoenixctms.org>
2023-02-16 16:06:31 +0100

    add github runner ctsms user to tomcat group

--- .github/workflows/download_masterdata.sh
+++ .github/workflows/download_masterdata.sh
@@ -8,10 +8,5 @@ wget --no-verbose https://api.github.com/repos/phoenixctms/master-data/tarball/m
 mkdir /ctsms/master_data
 tar -zxvf /ctsms/master-data.tar.gz -C /ctsms/master_data --strip-components 1
 rm /ctsms/master-data.tar.gz -f
-
-wget --no-verbose https://raw.githubusercontent.com/phoenixctms/install-debian/master/dbtool.sh -O /ctsms/dbtool.sh
-#chown ctsms:ctsms /ctsms/dbtool.sh
-#chmod 755 /ctsms/dbtool.sh
-
 chown ctsms:ctsms /ctsms -R
 chmod 777 /ctsms -R
