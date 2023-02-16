604831dd945a3818d70801a3af4fff6867622fdf
Rene Krenn <rkrenn@phoenixctms.org>
2023-02-16 10:25:09 +0100

    run jobrunner dbtool in /ctsms folder, launch from tomcat #1

--- .github/workflows/setup_tomcat.sh
+++ .github/workflows/setup_tomcat.sh
@@ -6,6 +6,7 @@ systemctl stop tomcat9
 #sed -r -i "s/^JAVA_OPTS.+/JAVA_OPTS=\"-server -Djava.awt.headless=true -Xms$XMS -Xmx$XMX -Xss$XSS -XX:+UseParallelGC -XX:MaxGCPauseMillis=1500 -XX:GCTimeRatio=9 -XX:+CMSClassUnloadingEnabled -XX:ReservedCodeCacheSize=$PERM\"/" /etc/default/tomcat9
 echo "CTSMS_PROPERTIES=$CTSMS_PROPERTIES" >>/etc/default/tomcat9
 echo "CTSMS_JAVA=$CTSMS_JAVA" >>/etc/default/tomcat9
+echo "VERSION=$VERSION" >>/etc/default/tomcat9
 sed -r -i "s|# Lifecycle|EnvironmentFile=/etc/default/tomcat9\\n\\n# Lifecycle|" /usr/lib/systemd/system/tomcat9.service
 sed -r -i "s|# Security|# Security\\nReadWritePaths=/ctsms/external_files/ /ctsms/bulk_processor/output/ /ctsms/ /tmp/|" /usr/lib/systemd/system/tomcat9.service
 chmod 755 /home/runner/work/ctsms/ctsms/web/target/ctsms-$VERSION.war
