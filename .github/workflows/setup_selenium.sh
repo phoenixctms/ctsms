c274bcb078a3ed63a2c6bca6f7b8f130f5869810
Rene Krenn <rkrenn@phoenixctms.org>
2023-02-22 12:10:06 +0100

    bump chromedriver version

--- .github/workflows/setup_selenium.sh
+++ .github/workflows/setup_selenium.sh
@@ -9,8 +9,8 @@
 #wget --no-verbose https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip
 #wget --no-verbose https://chromedriver.storage.googleapis.com/105.0.5195.52/chromedriver_linux64.zip
 #wget --no-verbose https://chromedriver.storage.googleapis.com/106.0.5249.61/chromedriver_linux64.zip
-#wget --no-verbose https://chromedriver.storage.googleapis.com/109.0.5414.25/chromedriver_linux64.zip
-wget --no-verbose https://chromedriver.storage.googleapis.com/108.0.5359.71/chromedriver_linux64.zip
+wget --no-verbose https://chromedriver.storage.googleapis.com/109.0.5414.25/chromedriver_linux64.zip
+#wget --no-verbose https://chromedriver.storage.googleapis.com/108.0.5359.71/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 
 mv chromedriver /usr/bin/chromedriver
