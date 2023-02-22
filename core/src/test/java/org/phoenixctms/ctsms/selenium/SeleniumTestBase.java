4fd78f3c7afa481ac3f017641d5b2a15ec9b5bce
Rene Krenn <rkrenn@phoenixctms.org>
2023-02-22 15:11:30 +0100

    rename "generated_pdfs.zip"

--- core/src/test/java/org/phoenixctms/ctsms/selenium/SeleniumTestBase.java
+++ core/src/test/java/org/phoenixctms/ctsms/selenium/SeleniumTestBase.java
@@ -1216,7 +1216,7 @@ public class SeleniumTestBase implements OutputLogger, ITestListener {
 			}
 		}
 		if (zip != null) {
-			String zipFilename = "generated_pdfs.zip";
+			String zipFilename = System.getProperty("ctsms.test.name") + ".zip";
 			getReportEmailSender().addEmailAttachment(zip.zipFiles(getTestDirectory()), Compress.ZIP_MIMETYPE_STRING, zipFilename);
 			zip.zipFiles(getTestDirectory(), zipFilename);
 			File zipFile = new File(getTestDirectory(), zipFilename);
