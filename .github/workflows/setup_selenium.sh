6659ecd8a33677aaf111e81d4cb13de3dba055b1
Rene Krenn <rkrenn@phoenixctms.org>
2023-09-08 03:12:27 +0200

    testrunner: print google chrome version

--- .github/workflows/setup_selenium.sh
+++ .github/workflows/setup_selenium.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+google-chrome --version
+
 #curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 #echo "deb [arch=amd64]  http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
 #apt-get -y update
