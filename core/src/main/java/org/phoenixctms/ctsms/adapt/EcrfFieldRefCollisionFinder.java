5c87f1392abd75443268db53fb6402d517f16256
Rene Krenn <rkrenn@phoenixctms.org>
2024-02-27 19:48:50 +0100

    eCRF field ref service impl

---
+++ core/src/main/java/org/phoenixctms/ctsms/adapt/EcrfFieldRefCollisionFinder.java
@@ -0,0 +1,46 @@
+package org.phoenixctms.ctsms.adapt;
+
+import java.util.Collection;
+
+import org.hibernate.LockMode;
+import org.phoenixctms.ctsms.domain.ECRF;
+import org.phoenixctms.ctsms.domain.ECRFDao;
+import org.phoenixctms.ctsms.domain.ECRFField;
+import org.phoenixctms.ctsms.domain.ECRFFieldDao;
+import org.phoenixctms.ctsms.exception.ServiceException;
+import org.phoenixctms.ctsms.util.CheckIDUtil;
+import org.phoenixctms.ctsms.vo.ECRFFieldInVO;
+
+public class EcrfFieldRefCollisionFinder extends CollisionFinder<ECRFFieldInVO, ECRFField, ECRF> {
+
+	private ECRFDao ecrfDao;
+	private ECRFFieldDao ecrfFieldDao;
+
+	public EcrfFieldRefCollisionFinder(ECRFDao ecrfDao, ECRFFieldDao ecrfFieldDao) {
+		this.ecrfFieldDao = ecrfFieldDao;
+		this.ecrfDao = ecrfDao;
+	}
+
+	@Override
+	protected ECRF aquireWriteLock(ECRFFieldInVO in)
+			throws ServiceException {
+		return CheckIDUtil.checkEcrfId(in.getEcrfId(), ecrfDao, LockMode.PESSIMISTIC_WRITE);
+	}
+
+	@Override
+	protected boolean equals(ECRFFieldInVO in,
+			ECRFField existing) {
+		return in.getId().equals(existing.getId());
+	}
+
+	@Override
+	protected Collection<ECRFField> getCollidingItems(
+			ECRFFieldInVO in, ECRF root) {
+		return ecrfFieldDao.findByEcrfRef(in.getEcrfId(), in.getRef());
+	}
+
+	@Override
+	protected boolean isNew(ECRFFieldInVO in) {
+		return in.getId() == null;
+	}
+}
