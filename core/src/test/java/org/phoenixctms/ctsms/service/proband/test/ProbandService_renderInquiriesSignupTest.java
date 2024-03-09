d47a2781bf8f839f70b9953f80f2612959f3b1a5
Rene Krenn <rkrenn@phoenixctms.org>
2024-03-09 17:39:48 +0100

    renderInquiriesSignup() "inquiryTrialId" param - UML model #1

--- core/src/test/java/org/phoenixctms/ctsms/service/proband/test/ProbandService_renderInquiriesSignupTest.java
+++ core/src/test/java/org/phoenixctms/ctsms/service/proband/test/ProbandService_renderInquiriesSignupTest.java
@@ -11,7 +11,7 @@ import org.testng.annotations.Test;
  * Test case for method <code>renderInquiriesSignup</code> of service <code>ProbandService</code>.
  * </p>
  *
- * @see org.phoenixctms.ctsms.service.proband.ProbandService#renderInquiriesSignup(org.phoenixctms.ctsms.vo.AuthenticationVO, java.lang.Long, java.lang.Long, java.lang.Boolean)
+ * @see org.phoenixctms.ctsms.service.proband.ProbandService#renderInquiriesSignup(org.phoenixctms.ctsms.vo.AuthenticationVO, java.lang.Long, java.lang.Long, java.lang.Long, java.lang.Boolean)
  */
 @Test(groups={"service","ProbandService"})
 public class ProbandService_renderInquiriesSignupTest extends ProbandServiceBaseTest {
