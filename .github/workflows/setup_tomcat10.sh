7c60e07d609f8c3d84e6036f0ac6d9c8ac591113
Rene Krenn <rkrenn@phoenixctms.org>
2024-12-23 19:30:03 +0100

    debian12 github ci runner: JDK_JAVA_OPTIONS with add-opens

--- .github/workflows/setup_tomcat10.sh
+++ .github/workflows/setup_tomcat10.sh
@@ -7,6 +7,7 @@ usermod --append --groups ctsms tomcat
 #sed -r -i "s/^JAVA_OPTS.+/JAVA_OPTS=\"-server -Djava.awt.headless=true -Xms$XMS -Xmx$XMX -Xss$XSS -XX:+UseParallelGC -XX:MaxGCPauseMillis=1500 -XX:GCTimeRatio=9 -XX:+CMSClassUnloadingEnabled -XX:ReservedCodeCacheSize=$PERM\"/" /etc/default/tomcat9
 echo "CTSMS_PROPERTIES=$CTSMS_PROPERTIES" >>/etc/default/tomcat10
 echo "CTSMS_JAVA=$CTSMS_JAVA" >>/etc/default/tomcat10
+echo "JDK_JAVA_OPTIONS=--add-opens java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.net=ALL-UNNAMED" >>/etc/default/tomcat10
 sed -r -i "s|# Lifecycle|EnvironmentFile=/etc/default/tomcat10\\n\\n# Lifecycle|" /usr/lib/systemd/system/tomcat10.service
 sed -r -i "s|# Security|# Security\\nReadWritePaths=/ctsms/external_files/ /ctsms/bulk_processor/output/ /ctsms/ /tmp/|" /usr/lib/systemd/system/tomcat10.service
 tar -xvzf /home/runner/work/ctsms/ctsms/.github/workflows/jakartaee-migration-1.0.8-bin.tar.gz -C /ctsms
