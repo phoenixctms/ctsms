fa97929ee097a0e2edd85066e5b7c8c8f8fdc4fe
Rene Krenn <rkrenn@phoenixctms.org>
2024-12-26 10:05:51 +0100

    debian12 github ci runner: use opens option in dbtool in jobs #3

--- .github/workflows/dbtool.sh
+++ .github/workflows/dbtool.sh
@@ -2,8 +2,7 @@
 #VERSION=$(grep -oP '<application.version>\K[^<]+' /home/runner/work/ctsms/ctsms/pom.xml)
 CTSMS_PROPERTIES=/ctsms/properties
 CTSMS_JAVA=/ctsms/java
-JDK_JAVA_OPTIONS=--add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED
 
 #$JAVA_HOME/bin/java -DCTSMS_PROPERTIES="$CTSMS_PROPERTIES" -DCTSMS_JAVA="$CTSMS_JAVA" -Dfile.encoding=Cp1252 -Djava.awt.headless=true -classpath /home/runner/work/ctsms/ctsms/web/target/ctsms-$VERSION/WEB-INF/lib/ctsms-core-$VERSION.jar:/home/runner/work/ctsms/ctsms/web/target/ctsms-$VERSION/WEB-INF/lib/* org.phoenixctms.ctsms.executable.DBTool $*
 
-$JAVA_HOME/bin/java -DCTSMS_PROPERTIES="$CTSMS_PROPERTIES" -DCTSMS_JAVA="$CTSMS_JAVA" -Dfile.encoding=Cp1252 -Djava.awt.headless=true -classpath /home/runner/work/ctsms/ctsms/web/target/ctsms-1.8.1/WEB-INF/lib/ctsms-core-1.8.1.jar:/home/runner/work/ctsms/ctsms/web/target/ctsms-1.8.1/WEB-INF/lib/* org.phoenixctms.ctsms.executable.DBTool $*
+$JAVA_HOME/bin/java -DCTSMS_PROPERTIES="$CTSMS_PROPERTIES" -DCTSMS_JAVA="$CTSMS_JAVA" -Dfile.encoding=Cp1252 --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED -Djava.awt.headless=true -classpath /home/runner/work/ctsms/ctsms/web/target/ctsms-1.8.1/WEB-INF/lib/ctsms-core-1.8.1.jar:/home/runner/work/ctsms/ctsms/web/target/ctsms-1.8.1/WEB-INF/lib/* org.phoenixctms.ctsms.executable.DBTool $*
