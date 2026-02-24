9965fe7d9594346e69448d0df98c6f1c85121f4e
Rene Krenn <rkrenn@phoenixctms.org>
2026-02-24 04:38:00 +0100

    removed hardcoded version from dbtool.sh

--- .github/workflows/dbtool.sh
+++ .github/workflows/dbtool.sh
@@ -5,4 +5,7 @@ CTSMS_JAVA=/ctsms/java
 
 #$JAVA_HOME/bin/java -DCTSMS_PROPERTIES="$CTSMS_PROPERTIES" -DCTSMS_JAVA="$CTSMS_JAVA" -Dfile.encoding=Cp1252 -Djava.awt.headless=true -classpath /home/runner/work/ctsms/ctsms/web/target/ctsms-$VERSION/WEB-INF/lib/ctsms-core-$VERSION.jar:/home/runner/work/ctsms/ctsms/web/target/ctsms-$VERSION/WEB-INF/lib/* org.phoenixctms.ctsms.executable.DBTool $*
 
-$JAVA_HOME/bin/java -DCTSMS_PROPERTIES="$CTSMS_PROPERTIES" -DCTSMS_JAVA="$CTSMS_JAVA" -Dfile.encoding=Cp1252 --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED -Djava.awt.headless=true -classpath /home/runner/work/ctsms/ctsms/web/target/ctsms-1.9.0/WEB-INF/lib/ctsms-core-1.9.0.jar:/home/runner/work/ctsms/ctsms/web/target/ctsms-1.9.0/WEB-INF/lib/* org.phoenixctms.ctsms.executable.DBTool $*
+CORE_JAR=$(ls /home/runner/work/ctsms/ctsms/web/target/ctsms-*/WEB-INF/lib/ctsms-core-*.jar)
+LIB_JARS=$(ls /home/runner/work/ctsms/ctsms/web/target/ctsms-*/WEB-INF/lib/*.jar | tr '\n' ':')
+
+$JAVA_HOME/bin/java -DCTSMS_PROPERTIES="$CTSMS_PROPERTIES" -DCTSMS_JAVA="$CTSMS_JAVA" -Dfile.encoding=Cp1252 --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED -Djava.awt.headless=true -classpath $CORE_JAR:$LIB_JARS org.phoenixctms.ctsms.executable.DBTool $*
