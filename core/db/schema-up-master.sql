323eac264e35489f8eeb8ff0b9851e3444f70e3a
Rene Krenn <rkrenn@phoenixctms.org>
2026-03-23 04:26:11 +0100

    massmail sms: html, phone2email address format - db changes

--- core/db/schema-up-master.sql
+++ core/db/schema-up-master.sql
@@ -11,5 +11,19 @@ if get_database_version() < '010901002' then
   
 end if;
 
+if get_database_version() < '010901003' then
+
+  ALTER TABLE MASS_MAIL ADD COLUMN HTML BOOLEAN;
+  UPDATE MASS_MAIL SET HTML = 't';
+  ALTER TABLE MASS_MAIL ALTER HTML SET NOT NULL;
+
+  alter table MASS_MAIL add column PHONE_TO_EMAIL_FORMAT CHARACTER VARYING(1024);
+  
+  ALTER TABLE MASS_MAIL ALTER SUBJECT_FORMAT DROP NOT NULL;
+  
+  perform set_database_version('010901003');
+  
+end if;
+
 end
 $$;
