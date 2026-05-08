758ad002540aecd84d844e3492333f27767280de
Rene Krenn <rkrenn@phoenixctms.org>
2026-05-08 13:58:08 +0200

    fix infinite loops when detecting empty JavaScript expressions #1

--- common/src/main/java/org/phoenixctms/ctsms/util/JavaScriptCompressor.java
+++ common/src/main/java/org/phoenixctms/ctsms/util/JavaScriptCompressor.java
@@ -8,22 +8,26 @@ import com.google.javascript.jscomp.SourceFile;
 public class JavaScriptCompressor {
 
 	public static String compress(String code) {
-		Compiler compiler = new Compiler();
-		CompilerOptions options = new CompilerOptions();
-		// SIMPLE_OPTIMIZATIONS is best for a direct replacement of your code.
-		// It removes comments and whitespace but keeps your variable names.
-		CompilationLevel.SIMPLE_OPTIMIZATIONS.setOptionsForCompilationLevel(options);
-		// Define the "file" name for error reporting purposes
-		options.setEmitUseStrict(false);
-		SourceFile extern = SourceFile.fromCode("externs.js", "");
-		SourceFile input = SourceFile.fromCode("input.js", code);
-		// Run the compiler
-		compiler.compile(extern, input, options);
-		if (compiler.hasErrors()) {
-			// Handle malformed JS here
-			//System.err.println("JS Error: " + compiler.getErrors()[0].description);
-			return code; // Or throw an exception
+		if (!CommonUtil.isEmptyString(code)) {
+			Compiler compiler = new Compiler();
+			CompilerOptions options = new CompilerOptions();
+			// SIMPLE_OPTIMIZATIONS is best for a direct replacement of your code.
+			// It removes comments and whitespace but keeps your variable names.
+			CompilationLevel.SIMPLE_OPTIMIZATIONS.setOptionsForCompilationLevel(options);
+			// Define the "file" name for error reporting purposes
+			options.setEmitUseStrict(false);
+			SourceFile extern = SourceFile.fromCode("externs.js", "");
+			SourceFile input = SourceFile.fromCode("input.js", code);
+			// Run the compiler
+			compiler.compile(extern, input, options);
+			if (compiler.hasErrors()) {
+				// Handle malformed JS here
+				//System.err.println("JS Error: " + compiler.getErrors()[0].description);
+				return code; // Or throw an exception
+			}
+			return compiler.toSource();
+		} else {
+			return "";
 		}
-		return compiler.toSource();
 	}
 }
